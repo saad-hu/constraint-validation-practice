ed81f4098edc479a485694288c08ff32296273c1
Dodo <saad9191h@gmail.com>
2022-09-18 09:14:43 -0500

    javascript logic for checking and displaying error is complete

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
+//refernce to the form and all the inputs
 let form = document.querySelector('form');
 let email = document.querySelector('#email');
 let country = document.querySelector('#country');
-let zipCode = document.querySelector('#zipcode');
+let zipcode = document.querySelector('#zipcode');
 let password = document.querySelector('#password');
 let confirmPassword = document.querySelector('#confirm-password');
 
-email.addEventListener('input', () => {
-    checkEmail();
-})
+//adding an input event listener to each input control. when ever user types something, each input is checked against it's dedicated input checking function. those functions display the error/instructions
+email.addEventListener('input', checkEmail);
+country.addEventListener('input', checkCountry);
+zipcode.addEventListener('input', checkzipcode);
+password.addEventListener('input', checkPassword);
+confirmPassword.addEventListener('input',checkConfirmPassword);
 
+//adding event listener to the form. when submitted, checkAll function is run. if it returns false, that means that atleast one(or more) input is filled incorrectly. the form is not submitted in this scenario, otherwise it is submitted 
 form.addEventListener('submit', (event) => {
-    if(!checkAll()) {
+    if (!checkAll()) {
         event.preventDefault();
     }
-} )
+});
 
 //checks if the email input is corretly filled. if its not, adds text to the instruction span and returns false. otherwise, clears instruction span and returns true
 function checkEmail() {
     let emailError = document.querySelector('input#email + span');
     //email checking
-    if(email.validity.valueMissing) {
+    if (email.validity.valueMissing) {
         emailError.textContent = "Email is required";
         return false;
     }
-    else if(email.validity.typeMismatch) {
+    else if (email.validity.typeMismatch) {
         emailError.textContent = "Use email format with '@'";
         return false;
     }
-    else if(email.validity.tooShort) {
+    else if (email.validity.tooShort) {
         emailError.textContent = `${email.minLength} characters required. You entered ${email.value.length}`;
         return false;
     }
@@ -40,8 +45,8 @@ function checkEmail() {
 //checks if the country input is corretly filled. if its not, adds text to the instruction span and returns false. otherwise, clears instruction span and returns true
 function checkCountry() {
     let countryError = document.querySelector('input#country + span');
-    //email checking
-    if(country.validity.valueMissing) {
+    //country checking
+    if (country.validity.valueMissing) {
         countryError.textContent = "Country is required";
         return false;
     }
@@ -51,14 +56,68 @@ function checkCountry() {
     }
 }
 
-//returns true if all inputs are correctly filled. if even one is not, reurns false
+//checks if the zipcode input is corretly filled. if its not, adds text to the instruction span and returns false. otherwise, clears instruction span and returns true
+function checkzipcode() {
+    let zipcodeError = document.querySelector('input#zipcode + span');
+    //password checking
+
+    //a regular expression which checks if the zipcode contains numbers only.
+    //if not, /[^0-9]/.test(zipcode.value) return true
+    if (/[^0-9]/.test(zipcode.value)) {
+        zipcodeError.textContent = "Zipcode should contain numbers only";
+        return false;
+    }
+    else {
+        zipcodeError.textContent = "";
+        return true;
+    }
+}
+
+//checks if the password input is corretly filled. if its not, adds text to the instruction span and returns false. otherwise, clears instruction span and returns true
+function checkPassword() {
+    let passwordError = document.querySelector('input#password + span');
+    //Password should contain minimum eight characters, at least one uppercase letter, one lowercase letter and one number
+    let passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    
+    //password checking
+    if(password.validity.valueMissing) {
+        passwordError.textContent = "Password is required";
+        return false;
+    }
+    else if (!passwordRegEx.test(password.value)) {
+        passwordError.textContent = "Password should contain minimum eight characters, at least one uppercase letter, one lowercase letter and one number";
+        return false;
+    }
+    else {
+        passwordError.textContent = "";
+        return true;
+    }
+}
+
+//checks if the confirm password input is corretly filled. if its not, adds text to the instruction span and returns false. otherwise, clears instruction span and returns true
+function checkConfirmPassword() {
+    let confirmPasswordError = document.querySelector('input#confirm-password + span');
+
+    // confirm password checking
+    if(password.value !== confirmPassword.value) {
+        confirmPasswordError.textContent = "Password does not match";
+        return false;
+    }
+    else {
+        confirmPasswordError.textContent = "";
+        return true;
+    }
+}
+
+//returns true if all inputs are correctly filled. if even one is not, reurns false.
+//this function first runs all the check functions so that any errors in input can be displayed to the user. this function is when submitting the form
 function checkAll() {
     let emailEr = checkEmail();
     let countryEr = checkCountry();
+    let zipcodeEr = checkzipcode(); 
+    let passwordEr = checkPassword();
+    let confirmPasswordEr = checkConfirmPassword();
 
-    return (emailEr && countryEr);
+    console.log(emailEr,countryEr,zipcodeEr,passwordEr,confirmPasswordEr);
+    return (emailEr && countryEr && zipcodeEr && passwordEr && confirmPasswordEr);
 }
-
-
-
-
